11df6070053933ab0e31ee9d0a340741a54309f1
Arush <alan2.008y@gmail.com>
2026-01-05 19:32:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -281,7 +281,6 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     RESTART_TXT = """
 <b>{} Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
 
-⏰ Tɪᴍᴇ : <code>{}</code>
 🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
 🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code> v4.8 [ Sᴛᴀʙʟᴇ ]</code>
 </b>"""
